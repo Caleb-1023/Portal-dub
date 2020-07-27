989579a062bf38c6db78864353970493d0ef5b6d
okaleb106@gmail.com <okaleb106@gmail.com>
2020-07-27 10:49:29 +0100

    Hello

---
+++ app.js
@@ -0,0 +1,12 @@
+function validateForm() {
+    let X = document.forms["myPortal"]["userName"].value;
+    let Y = document.forms["myPortal"]["passWord"].value;
+    if ((X === null || X === "" || X === 'undefined' && Y === null || Y === "")) {
+        alert("This field must be filled!!!");
+        return false;
+    } 
+    else {
+        alert(`Hi ${X} and ${Y}`);
+        return true;
+    }
+}
